6dc79e06e3f4a3cfd81c9802def1fb7fccb2ad27
morzook <59170230+morzook@users.noreply.github.com>
2022-05-02 16:46:39 +0100

    new update

--- portfolio.js
+++ portfolio.js
@@ -1,21 +1,46 @@
-(() => {
-    const hamburger = document.getElementById("hamburger");
-    const menu = document.getElementById("overlay");
-    let open = false;
-  
-    const change = () => {
-      if (!open) {
-        hamburger.classList.add("open");
-        menu.classList.add("menu");
-      } else {
-        hamburger.classList.remove("open");
-        menu.classList.remove("menu");
-      }
-      open = !open;
-    };
-  
-    hamburger.addEventListener("click", change);
-  })();
-  
+// define all UI variable
+const navToggler = document.querySelector('.nav-toggler');
+const navMenu = document.querySelector('.site-navbar ul');
+const navLinks = document.querySelectorAll('.site-navbar a');
 
- 
+// load all event listners
+allEventListners();
+
+// functions of all event listners
+function allEventListners() {
+  // toggler icon click event
+  navToggler.addEventListener('click', togglerClick);
+  // nav links click event
+  navLinks.forEach( elem => elem.addEventListener('click', navLinkClick));
+}
+
+// togglerClick function
+function togglerClick() {
+  navToggler.classList.toggle('toggler-open');
+  navMenu.classList.toggle('open');
+}
+
+// navLinkClick function
+function navLinkClick() {
+  if(navMenu.classList.contains('open')) {
+    navToggler.click();
+  }
+}
+
+const items = document.querySelectorAll('.accordion button');
+
+function toggleAccordion() {
+  const itemToggle = this.getAttribute('aria-expanded');
+
+  for (i = 0; i < items.length; i++) {
+    items[i].setAttribute('aria-expanded', 'false');
+  }
+
+  if (itemToggle == 'false') {
+    this.setAttribute('aria-expanded', 'true');
+  }
+}
+
+items.forEach((item) => item.addEventListener('click', toggleAccordion));
+
+
